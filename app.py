a1ab96c39b29d16cb24da0f4963eb60efc11e22e
Bunty Kumar <92105094+bunty187@users.noreply.github.com>
2024-07-21 14:23:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,43 @@
+import os
+import sys
+import streamlit as st
+import nltk
+import yaml
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+
+class HiddenPrints:
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
+
+with HiddenPrints():
+    nltk.download("punkt")
+
+# Rest of your code follows
+
+import ast
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
-
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-
 from langchain.document_loaders import PyPDFDirectoryLoader
 from langchain_groq import ChatGroq
 from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import RunnablePassthrough, RunnableParallel
 from langchain_milvus.retrievers import MilvusCollectionHybridSearchRetriever
 from langchain_milvus.utils.sparse import BM25SparseEmbedding
-
-
-from langchain_core.runnables import RunnablePassthrough,RunnableParallel
-
 from langchain.chains import create_retrieval_chain
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain.chains.combine_documents import create_stuff_documents_chain
-
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
-# from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.output_parsers import StrOutputParser
-
-# from langchain.memory import ChatMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
-
-# from openai import OpenAI
-import streamlit as st
-import nltk                                                             
-import yaml                                                             
-
-from nltk import word_tokenize                                          
-
-from nltk.corpus import stopwords 
-
-nltk.download("punkt")
-
-import os
-
-# os.environ['GOOGLE_API_KEY'] = 'AIzaSyDvzWbyMBX-WIL-1pungOZ0XCOqd5tZaqw'
-os.environ['GROQ_API_KEY'] = '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'
-
-model = ChatGroq(model_name="Llama3-8b-8192")
-
-# from milvus import default_server
-
-# default_server.start()
-
-
 from pymilvus import (
     Collection,
     CollectionSchema,
@@ -60,11 +47,13 @@ from pymilvus import (
     connections,
 )
 
+# Set environment variables
+os.environ['GROQ_API_KEY'] = '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'
 
+model = ChatGroq(model_name="Llama3-8b-8192")
 
 CONNECTION_URI = "database/milvus_demo.db"
 connections.connect(uri=CONNECTION_URI)
-
 
 pk_field = "doc_id"
 dense_field = "dense_vector"
@@ -83,28 +72,19 @@ fields = [
     FieldSchema(name=text_field, dtype=DataType.VARCHAR, max_length=65_535),
 ]
 
-
 schema = CollectionSchema(fields=fields, enable_dynamic_field=False)
 collection = Collection(
     name="IntroductionToTheRaptors", schema=schema, consistency_level="Strong"
 )
-
-
-
-import ast
 
 # Read list from the file
 with open('list.txt', 'r') as file:
     contents = file.read()
     all_texts = ast.literal_eval(contents)
 
+sparse_embedding_func = BM25SparseEmbedding(corpus=all_texts)
+embd = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-MiniLm-L3-v2")
 
-sparse_embedding_func = BM25SparseEmbedding(corpus=all_texts)   
-
-embd=HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-MiniLm-L3-v2")
-
-
-# from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 sparse_search_params = {"metric_type": "IP"}
 dense_search_params = {"metric_type": "IP", "params": {}}
 retriever = MilvusCollectionHybridSearchRetriever(
@@ -122,15 +102,12 @@ chat_template = ChatPromptTemplate.from_messages([
     # System Message Prompt Template
     SystemMessage(content="""You are a Helpful AI Bot.
     You take the context and question from user. Your answer should be based on the specific context."""),
-
     # Human Message Prompt Template
     HumanMessagePromptTemplate.from_template("""Answer the question based on the given context.
     Context:
     {context}
-
     Question:
     {question}
-
     Answer: """)
 ])
 
